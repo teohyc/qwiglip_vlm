8f27ef8079ea50fabc6e20cf72cc42d45854407c
teohyc <teohyc2018@gmail.com>
2026-04-04 08:52:56 +0800

    new residual mlp projector

--- test_infer.py
+++ test_infer.py
@@ -55,54 +55,56 @@ dataset = load_from_disk(DATASET_PATH)
 dataset = dataset.train_test_split(test_size=0.05, seed=42)
 val_dataset = dataset["test"]
 
-sample = random.choice(val_dataset)
+#randomly select 7 samples from validation set for testing
+for _ in range(7):
+    sample = random.choice(val_dataset)
 
-print("Caption:", sample["messages"][1]["content"])
-print("Image Path:", sample["image_path"])
+    print("Caption:", sample["messages"][1]["content"])
+    print("Image Path:", sample["image_path"])
 
-image = Image.open(sample["image_path"]).convert("RGB")
+    image = Image.open(sample["image_path"]).convert("RGB")
 
-#show image
-plt.imshow(image)
-plt.axis("off")
-plt.title("Input Image")
-plt.show()
-
-
-#input preparation
-image_block = " ".join(["<image>"] * NUM_IMAGE_TOKENS)
-
-prompt = (
-    f"USER: {image_block}\n"
-    "Describe the image in 2-3 short sentences. "
-    "Be accurate and only mention clearly visible details.\n"
-    "ASSISTANT:"
-)
-
-inputs = processor(images=image, return_tensors="pt")
-pixel_values = inputs["pixel_values"].to(DEVICE)
-
-tokenized = tokenizer(prompt, return_tensors="pt").to(DEVICE)
+    #show image
+    plt.imshow(image)
+    plt.axis("off")
+    plt.title("Input Image")
+    plt.show()
 
 
-#generate
-with torch.no_grad():
-    output_ids = model.generate(
-        pixel_values=pixel_values,
-        input_ids=tokenized["input_ids"],
-        attention_mask=tokenized["attention_mask"],
-        max_new_tokens=60,
-        temperature=0.7,
-        top_p=0.9,
-        repetition_penalty=1.2,
-        no_repeat_ngram_size=3,
+    #input preparation
+    image_block = " ".join(["<image>"] * NUM_IMAGE_TOKENS)
+
+    prompt = (
+        f"USER: {image_block}\n"
+        "Describe the image in 2-3 short sentences. "
+        "Be accurate and only mention clearly visible details.\n"
+        "ASSISTANT:"
     )
 
-output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    inputs = processor(images=image, return_tensors="pt")
+    pixel_values = inputs["pixel_values"].to(DEVICE)
 
-#cleaning output
-sentences = output_text.split(".")
-output_text = ".".join(sentences[:3]).strip() + "."
+    tokenized = tokenizer(prompt, return_tensors="pt").to(DEVICE)
 
-print("\n=== Generated Caption ===")
-print(output_text)
+
+    #generate
+    with torch.no_grad():
+        output_ids = model.generate(
+            pixel_values=pixel_values,
+            input_ids=tokenized["input_ids"],
+            attention_mask=tokenized["attention_mask"],
+            max_new_tokens=60,
+            temperature=0.7,
+            top_p=0.9,
+            repetition_penalty=1.2,
+            no_repeat_ngram_size=3,
+        )
+
+    output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+    #cleaning output
+    sentences = output_text.split(".")
+    output_text = ".".join(sentences[:3]).strip() + "."
+
+    print("\n=== Generated Caption ===")
+    print(output_text)
